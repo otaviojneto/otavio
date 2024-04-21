ae6059ba7f63eab5fcae87a4fca7d69bae113e63
Victor Marques <victor.marques@niara.tech>
2024-04-21 20:12:37 -0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import logo from "./assets/logo.svg";
 
 const App = () => {
   return (
-    <body className="p-5">
-      <img className="h-24 w-50" src={logo} alt="" />
+    <body className="md: p-5">
+      <nav className="flex justify-between">
+        <img className="h-10 " src={logo} alt="" />{" "}
+        <ul className="flex items-center gap-2">
+          <li className="font-bold">
+            <a href="#">sobre</a>
+          </li>
+        </ul>
+      </nav>
       <h1 className="text-3xl font-bold underline text-center">Hello</h1>
     </body>
   );
