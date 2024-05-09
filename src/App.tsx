0340ae6bab94b8138a9b961b3a35ac16a82bebce
Victor Marques <victor.marques@niara.tech>
2024-05-09 20:20:58 -0300

    estrutura com tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import logo from "./assets/logo.svg";
+import computer from "./assets/computer.jpg";
 
 const App = () => {
   return (
     <body className="md: p-5">
-      <nav className="flex justify-between">
+      <nav className="flex justify-between nav">
         <img className="h-10 " src={logo} alt="" />{" "}
         <ul className="flex items-center gap-2">
           <li className="font-bold">
@@ -11,7 +12,15 @@ const App = () => {
           </li>
         </ul>
       </nav>
-      <h1 className="text-3xl font-bold underline text-center">Hello</h1>
+      <section>
+        <div>
+          <h1 className="text-3xl font-bold underline text-center">
+            Frontend Developer
+          </h1>
+
+          <img src={computer} alt="img" />
+        </div>
+      </section>
     </body>
   );
 };
